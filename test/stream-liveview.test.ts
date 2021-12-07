7026110058cb55734e6830ace96e48ff4762186c
Nicolas Mondain <nicolas@sharingbox.fr>
2021-12-07 12:35:36 +0100

    stream-liveview.test modifications (startLiveview, complete shoot process)

--- test/stream-liveview.test.ts
+++ test/stream-liveview.test.ts
@@ -1,8 +1,10 @@
-import {getLivefeedStatusResponse} from '../src/@types/http-methods';
+import {httpResponse} from '@sharingbox/http-status/src/@types/http-status';
 
 import * as chai from 'chai';
+import {AxiosError} from 'axios';
 import cameras from './cameras.test';
 import chaiAsPromised from 'chai-as-promised';
+import httpStatus from '@sharingbox/http-status/dist/browser';
 import httpStream from '../src/stream/stream-http';
 
 import 'mocha';
@@ -13,9 +15,13 @@ chai.should();
 const CAMERA_TYPE:string = process.env.CAMERA || 'WEBCAM';
 const CAMERA             = cameras[CAMERA_TYPE] || cameras.WEBCAM;
 
+const DELAY_BETWEEN_STARTLIVEVIEW_AND_GETLIVEFEEDSTATUS = 2000;
+const DELAY_MAX_FOR_LIVEFEED_ANALYSIS                   = 5000;
+
 describe(`stream-liveview ${CAMERA_TYPE}`, function streamLiveview(){
 
 	this.slow(0); // eslint-disable-line no-invalid-this
+	this.timeout(DELAY_MAX_FOR_LIVEFEED_ANALYSIS); // eslint-disable-line no-invalid-this
 
 	context(`standard HTTP calls with the correct parameters ${CAMERA_TYPE}`, () => {
 
@@ -33,14 +39,41 @@ describe(`stream-liveview ${CAMERA_TYPE}`, function streamLiveview(){
 
 		});
 
-		it('getLivefeedStatus should return an object (status: "success") if livefeed is on', () => {
+		it('getLivefeedStatus should return an object (status: "success") if livefeed is on', (done) => {
 
 			httpStream.startLiveView(CAMERA)
-			.then(() => httpStream.getLivefeedStatus(CAMERA))
-			.then((response: getLivefeedStatusResponse) => {
+			.then(() => new Promise<void>((resolve) => {
 
-				httpStream.stopLiveView(CAMERA);
-				chai.expect(response.status).to.equal('success');
+				setTimeout(() => {
+
+					resolve();
+
+				}, DELAY_BETWEEN_STARTLIVEVIEW_AND_GETLIVEFEEDSTATUS);
+
+			}))
+			.then(() => httpStream.getLivefeedStatus(CAMERA))
+			.then((_getLivefeedStatus: httpResponse) => {
+
+				chai.expect(_getLivefeedStatus.data.status).to.equal('success');
+
+				httpStream.stopLiveView(CAMERA).then(() => {
+
+					done();
+
+				});
+
+			})
+			.catch((e: AxiosError) => {
+
+				let error = e;
+
+				if(error.isAxiosError && error.response){
+
+					error = httpStatus.formatResponse(error.response.status, error.response.statusText, error.response.data);
+
+				}
+
+				console.log(error);
 
 			});
 
